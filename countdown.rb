a094b1f54caf1ea9988d5c727951d9db81464e9f
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2020-05-08 23:46:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0 
+    puts "#{number} SECONDS!"
+    counter -= log10
+  end
+  
+  puts "HAPPY NEW YEAR!"
 end
+
+
